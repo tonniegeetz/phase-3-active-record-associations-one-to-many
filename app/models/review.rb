0a2c0c0f39b042214bf79c954d9c1f3bc649ceff
tonniegeetz <tonniegeetz@gmail.com>
2024-07-03 21:33:03 +0300

    relationships

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
-  
+  # # a review belongs to a game
+  # def game
+  #   # self is the review instance
+  #   Game.find(game_id)
+  # end
+
+  belongs_to :game
 end
